398ffffc06704ab30726fedc2108954aad126d57
Tirano2150 <tim.ellis@gft.com>
2020-07-14 13:37:34 +0100

    Test code

--- jenkins/JCasC/jobs/hello_world.groovy
+++ jenkins/JCasC/jobs/hello_world.groovy
@@ -1,10 +1,25 @@
 pipeline {
-    agent { label 'master' }
-    stages {
-        stage('build') {
-            steps {
-                echo "Hello World!"
-            }
-        }
+  agent any
+
+  environment {
+    // FOO will be available in entire pipeline
+    FOO = "PIPELINE"
+  }
+
+  stages {
+    stage("local") {
+      environment {
+        // BAR will only be available in this stage
+        BAR = "STAGE"
+      }
+      steps {
+        sh 'echo "FOO is $FOO and BAR is $BAR"'
+      }
     }
+    stage("global") {
+      steps {
+        sh 'echo "FOO is $FOO and BAR is $BAR"'
+      }
+    }
+  }
 }
