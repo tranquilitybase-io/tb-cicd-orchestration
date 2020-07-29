b0af6ffc60821341cc458df02dad267a607608c0
Tirano2150 <tim.ellis@gft.com>
2020-07-29 15:46:00 +0100

    Added hack for gcloud creds

--- jenkins/JCasC/jobs/tibco.groovy
+++ jenkins/JCasC/jobs/tibco.groovy
@@ -3,9 +3,10 @@ job('Tibco') {
   steps {
     shell (
       '''
-      cd /usr/share/jenkins/synapse/
-      ls
-      gcloud
+cd /usr/share/jenkins/synapse/
+export GOOGLE_APPLICATION_CREDENTIALS=credentials.json
+ls
+gcloud
       '''
       )
   }
