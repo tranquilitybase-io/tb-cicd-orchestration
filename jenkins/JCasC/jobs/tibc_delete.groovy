6c32a861ed5aeb7e5e205d278ac2e51dda8dd8ee
Tirano2150 <tim.ellis@gft.com>
2020-07-30 15:50:40 +0100

    Udpated destroy job to not use make file

--- jenkins/JCasC/jobs/tibc_delete.groovy
+++ jenkins/JCasC/jobs/tibc_delete.groovy
@@ -6,7 +6,7 @@ job('Tibco Delete') {
 cd /usr/share/jenkins/synapse/
 export GOOGLE_APPLICATION_CREDENTIALS=credentials.json
 ls
-make destroy
+terraform destroy -var-file="/vars/dev.tfvar" -auto-apply
       '''
       )
   }
