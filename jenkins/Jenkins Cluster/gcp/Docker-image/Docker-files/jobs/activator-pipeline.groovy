c97cdf52071614c0cb7774bba09cb9f584792725
zain-GFT <64900858+zain-GFT@users.noreply.github.com>
2020-10-05 14:38:39 +0100

    Create activator-pipeline.groovy

---
+++ jenkins/Jenkins Cluster/gcp/Docker-image/Docker-files/jobs/activator-pipeline.groovy
@@ -0,0 +1,24 @@
+pipelineJob("activator-pipeline") {
+	description()
+	keepDependencies(false)
+	authenticationToken('secret')
+	parameters {
+		stringParam("repourl", "", "")
+		stringParam("projectid", "", "")
+		stringParam("activator_params", "size=100,region=europe-west2", "")
+		stringParam("job_unique_id", "", "")
+	}
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url 'https://github.com/tranquilitybase-io/tb-activator-gft-base.git'
+					}
+				}
+			}
+			scriptPath("Jenkinsfile")
+		}
+	}
+	disabled(false)
+}
